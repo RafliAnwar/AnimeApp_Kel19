f3cb1c3a559996c281615a4f6087277641d5dac6
RafliAnwar <rafli.anwar2001@gmail.com>
2021-12-26 23:15:35 +0700

    change appbar color

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -32,181 +32,193 @@ class _HomePageState extends State<HomePage> {
         // shape: RoundedRectangleBorder(
         //     borderRadius: BorderRadius.all(Radius.circular(30))),
         title: const Text('MyAnimeList'),
-        backgroundColor: Color(0xff063970),
+        // backgroundColor: Color(0xff063970),
+        backgroundColor: Colors.black,
       ),
-      body: Column(
-        children: <Widget>[
-          Text(
-            'Top Airing',
-            style:
-                GoogleFonts.poppins(fontSize: 18, fontWeight: FontWeight.bold),
-          ),
-          /////////////////////////////////////////////////////////////////FIRST LISTVIEW
-          FutureBuilder(
-              future: showsFirst,
-              builder: (context, AsyncSnapshot<List<ShowFirst>> snapshot) {
-                if (snapshot.hasData) {
-                  return Container(
-                    height: 200,
-                    child: ListView.builder(
-                      itemCount: snapshot.data!.length,
-                      shrinkWrap: true,
-                      scrollDirection: Axis.horizontal,
-                      itemBuilder: (BuildContext context, int index) {
-                        return GestureDetector(
-                          //TopAiring //Navigasi
-                          onTap: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => DetailFirst(
-                                    item: snapshot.data![index].malId,
-                                    title: snapshot.data![index].title),
-                              ),
-                            );
-                          },
-                          child: ClipRRect(
-                            borderRadius: BorderRadius.all(Radius.circular(10)),
-                            child: Container(
-                              color: Colors.brown[50],
-                              width: 150,
-                              height: 200,
-                              margin: EdgeInsets.only(right: 3),
-                              padding: EdgeInsets.all(5),
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Stack(
-                                    children: [
-                                      Image.network(
-                                        snapshot.data![index].imageUrl,
-                                        width: 150,
-                                        height: 160,
-                                        fit: BoxFit.cover,
-                                      ),
-                                      Padding(
-                                        padding: const EdgeInsets.all(2),
-                                        child: Align(
-                                          alignment: Alignment.topLeft,
-                                          child: Container(
-                                            width: 60,
-                                            height: 30,
-                                            decoration: BoxDecoration(
-                                                color: Colors.black
-                                                    .withOpacity(0.4),
-                                                borderRadius: BorderRadius.only(
-                                                    bottomRight:
-                                                        Radius.circular(30),
-                                                    bottomLeft:
-                                                        Radius.circular(30),
-                                                    topLeft:
-                                                        Radius.circular(30),
-                                                    topRight:
-                                                        Radius.circular(30))),
-                                            child: Row(
-                                              mainAxisAlignment:
-                                                  MainAxisAlignment.center,
-                                              crossAxisAlignment:
-                                                  CrossAxisAlignment.center,
-                                              children: [
-                                                Image.asset(
-                                                  'assets/star.png',
-                                                  width: 15,
-                                                  height: 13,
-                                                ),
-                                                Text(
-                                                    '${snapshot.data![index].score}',
-                                                    style: GoogleFonts.poppins(
-                                                        fontSize: 10,
-                                                        color: Colors.white))
-                                              ],
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Text(
+              'Top Airing',
+              style: GoogleFonts.poppins(
+                  fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            /////////////////////////////////////////////////////////////////FIRST LISTVIEW
+            FutureBuilder(
+                future: showsFirst,
+                builder: (context, AsyncSnapshot<List<ShowFirst>> snapshot) {
+                  if (snapshot.hasData) {
+                    return Container(
+                      height: 200,
+                      child: ListView.builder(
+                        itemCount: snapshot.data!.length,
+                        shrinkWrap: true,
+                        scrollDirection: Axis.horizontal,
+                        itemBuilder: (BuildContext context, int index) {
+                          return GestureDetector(
+                            //TopAiring //Navigasi
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => DetailFirst(
+                                      item: snapshot.data![index].malId,
+                                      title: snapshot.data![index].title),
+                                ),
+                              );
+                            },
+                            child: ClipRRect(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(10)),
+                              child: Container(
+                                color: Colors.brown[50],
+                                width: 150,
+                                height: 200,
+                                margin: EdgeInsets.only(right: 3),
+                                padding: EdgeInsets.all(5),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Stack(
+                                      children: [
+                                        Image.network(
+                                          snapshot.data![index].imageUrl,
+                                          width: 150,
+                                          height: 160,
+                                          fit: BoxFit.cover,
+                                        ),
+                                        Padding(
+                                          padding: const EdgeInsets.all(2),
+                                          child: Align(
+                                            alignment: Alignment.topLeft,
+                                            child: Container(
+                                              width: 60,
+                                              height: 30,
+                                              decoration: BoxDecoration(
+                                                  color: Colors.black
+                                                      .withOpacity(0.4),
+                                                  borderRadius:
+                                                      BorderRadius.only(
+                                                          bottomRight:
+                                                              Radius.circular(
+                                                                  30),
+                                                          bottomLeft:
+                                                              Radius.circular(
+                                                                  30),
+                                                          topLeft: Radius
+                                                              .circular(30),
+                                                          topRight:
+                                                              Radius.circular(
+                                                                  30))),
+                                              child: Row(
+                                                mainAxisAlignment:
+                                                    MainAxisAlignment.center,
+                                                crossAxisAlignment:
+                                                    CrossAxisAlignment.center,
+                                                children: [
+                                                  Image.asset(
+                                                    'assets/star.png',
+                                                    width: 15,
+                                                    height: 13,
+                                                  ),
+                                                  Text(
+                                                      '${snapshot.data![index].score}',
+                                                      style:
+                                                          GoogleFonts.poppins(
+                                                              fontSize: 10,
+                                                              color:
+                                                                  Colors.white))
+                                                ],
+                                              ),
                                             ),
                                           ),
-                                        ),
-                                      )
-                                    ],
-                                  ),
-                                  SizedBox(
-                                    height: 10,
-                                  ),
-                                  Center(
-                                    child: Text(
-                                      snapshot.data![index].title,
-                                      style: GoogleFonts.poppins(
-                                          fontSize: 13,
-                                          fontWeight: FontWeight.bold),
-                                      overflow: TextOverflow.ellipsis,
+                                        )
+                                      ],
                                     ),
-                                  )
-                                ],
+                                    SizedBox(
+                                      height: 10,
+                                    ),
+                                    Center(
+                                      child: Text(
+                                        snapshot.data![index].title,
+                                        style: GoogleFonts.poppins(
+                                            fontSize: 13,
+                                            fontWeight: FontWeight.bold),
+                                        overflow: TextOverflow.ellipsis,
+                                      ),
+                                    )
+                                  ],
+                                ),
                               ),
                             ),
+                          );
+                        },
+                        // children: [
+                        //   TopAiring(),
+                        // ],
+                      ),
+                    );
+                  } else if (snapshot.hasError) {
+                    return const Center(child: Text('Something went wrong :('));
+                  }
+                  return const CircularProgressIndicator();
+                }),
+
+            Text(
+              'Top Anime of All Time',
+              style: GoogleFonts.poppins(
+                  fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            //////////////////////////////////////////////////////////SECOND LISTVIEW
+            FutureBuilder(
+              future: shows,
+              builder: (context, AsyncSnapshot<List<Show>> snapshot) {
+                if (snapshot.hasData) {
+                  return Flexible(
+                    child: ListView.builder(
+                      physics: NeverScrollableScrollPhysics(),
+                      shrinkWrap: true,
+                      itemCount: snapshot.data!.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        return Card(
+                          shape: RoundedRectangleBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(15))),
+                          color: Colors.grey[100],
+                          child: ListTile(
+                            leading: CircleAvatar(
+                              radius: 30,
+                              backgroundImage:
+                                  NetworkImage(snapshot.data![index].imageUrl),
+                            ),
+                            title: Text(snapshot.data![index].title),
+                            subtitle:
+                                Text('Score: ${snapshot.data![index].score}'),
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => DetailPage(
+                                      //Navigasi
+                                      item: snapshot.data![index].malId,
+                                      title: snapshot.data![index].title),
+                                ),
+                              );
+                            },
                           ),
                         );
                       },
-                      // children: [
-                      //   TopAiring(),
-                      // ],
                     ),
                   );
                 } else if (snapshot.hasError) {
                   return const Center(child: Text('Something went wrong :('));
                 }
                 return const CircularProgressIndicator();
-              }),
-
-          Text(
-            'Top Anime of All Time',
-            style:
-                GoogleFonts.poppins(fontSize: 18, fontWeight: FontWeight.bold),
-          ),
-          //////////////////////////////////////////////////////////SECOND LISTVIEW
-          FutureBuilder(
-            future: shows,
-            builder: (context, AsyncSnapshot<List<Show>> snapshot) {
-              if (snapshot.hasData) {
-                return Expanded(
-                  child: ListView.builder(
-                    shrinkWrap: true,
-                    itemCount: snapshot.data!.length,
-                    itemBuilder: (BuildContext context, int index) {
-                      return Card(
-                        shape: RoundedRectangleBorder(
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(15))),
-                        color: Colors.grey[100],
-                        child: ListTile(
-                          leading: CircleAvatar(
-                            radius: 30,
-                            backgroundImage:
-                                NetworkImage(snapshot.data![index].imageUrl),
-                          ),
-                          title: Text(snapshot.data![index].title),
-                          subtitle:
-                              Text('Score: ${snapshot.data![index].score}'),
-                          onTap: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => DetailPage(
-                                    //Navigasi
-                                    item: snapshot.data![index].malId,
-                                    title: snapshot.data![index].title),
-                              ),
-                            );
-                          },
-                        ),
-                      );
-                    },
-                  ),
-                );
-              } else if (snapshot.hasError) {
-                return const Center(child: Text('Something went wrong :('));
-              }
-              return const CircularProgressIndicator();
-            },
-          ),
-        ],
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -236,8 +248,8 @@ class Show {
 }
 
 Future<List<Show>> fetchShows() async {
-  final response =
-      await http.get(Uri.parse('https://api.jikan.moe/v3/top/anime/1'));
+  final response = await http.get(Uri.parse(
+      'https://raflianwar.github.io/MALmockJson/mal.json')); //FakeAPI via GIT
 
   if (response.statusCode == 200) {
     var topShowsJson = jsonDecode(response.body)['top'] as List;
